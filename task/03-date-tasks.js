43b607a44c59a3294473b3068e1b72bd091920ac
Roman <casper00941@gmail.com>
2016-03-21 12:05:45 +0300

    Solve date tasks

--- task/03-date-tasks.js
+++ task/03-date-tasks.js
@@ -22,7 +22,7 @@
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
 function parseDataFromRfc2822(value) {
-   throw new Error('Not implemented');
+    return new Date(value);
 }
 
 /**
@@ -37,7 +37,7 @@ function parseDataFromRfc2822(value) {
  *    '2016-01-19T08:07:37Z' => Date()
  */
 function parseDataFromIso8601(value) {
-   throw new Error('Not implemented');
+    return new Date(value);
 }
 
 
@@ -56,7 +56,10 @@ function parseDataFromIso8601(value) {
  *    Date(2015,1,1)    => false
  */
 function isLeapYear(date) {
-   throw new Error('Not implemented');
+    var year = date.getFullYear();
+    if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+        return true;
+    return false;
 }
 
 
@@ -76,7 +79,7 @@ function isLeapYear(date) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
 function timeSpanToString(startDate, endDate) {
-   throw new Error('Not implemented');
+    return new Date(endDate - startDate).toISOString().slice(-13, -1);
 }
 
 
@@ -94,7 +97,15 @@ function timeSpanToString(startDate, endDate) {
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
 function angleBetweenClockHands(date) {
-    throw new Error('Not implemented');
+    const clockPartInMin = 1 / 60;
+    const clockPartInHour = 1 / 12;
+
+    var mins = date.getUTCMinutes();
+    var hours = date.getUTCHours() % 12 + (clockPartInMin * mins);
+    var angle = Math.abs(2 * Math.PI * (clockPartInMin * mins - clockPartInHour * hours));
+    while (angle > Math.PI)
+        angle -= Math.PI;
+    return angle;
 }
 
 
